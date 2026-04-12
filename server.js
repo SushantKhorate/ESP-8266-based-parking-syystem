4bdc285de13348357221fa4fedd99f149537aa0d
SushantKhorate <sushantkhorate05@gmail.com>
2026-04-12 23:40:20 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,28 +4,28 @@ const app = express();
 app.use(express.json());
 app.use(express.static("public"));
 
-let slots = { s1: 1, s2: 1, s3: 1 };
+let sensors = { s1: 1, s2: 1, s3: 1 };
 
-// ESP updates this
 app.post("/update", (req, res) => {
-  slots = req.body;
+  sensors = req.body;
   res.send("OK");
 });
 
-// Frontend reads this
 app.get("/data", (req, res) => {
-  // Mapping logic (you decide meaning)
 
-  let busVacant = slots.s1 ? 3 : 2;
-  let miniVacant = slots.s2 ? 3 : 2;
-  let carVacant = slots.s3 ? 3 : 2;
+  // Create 12 slots (default = empty)
+  let slots = {
+    P1: 1, P2: 1, P3: 1, P4: 1,
+    P5: 1, P6: 1, P7: 1, P8: 1,
+    P9: 1, P10: 1, P11: 1, P12: 1
+  };
 
-  res.json({
-    bus: busVacant,
-    mini: miniVacant,
-    car: carVacant,
-    raw: slots
-  });
+  // Map real sensors
+  slots.P1 = sensors.s1;
+  slots.P6 = sensors.s2;
+  slots.P10 = sensors.s3;
+
+  res.json(slots);
 });
 
 app.listen(3000, () => console.log("Server running"));
